99ebcd3cf127cc5706596a63a01a2226a462efac
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-07-02 19:08:55 -0300

    Componentes
    Ciração e exportação de componentes]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Gabriella'
@@ -16,6 +17,7 @@ function App() {
      <p>Olá, {newName}</p>
      <p>Soma: {sum(2, 1)}</p>
      <img src={url} alt="Minha imagem" />
+     <HelloWorld />
     </div>
   );
 }
