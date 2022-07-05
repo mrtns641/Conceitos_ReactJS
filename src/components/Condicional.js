b70c8cf4f66328bdc0346cd266e55570bafe0843
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-07-05 14:03:34 -0300

    Renderização Condicional
    Renderização condicional exemplificada no componente Condicional.js

---
+++ src/components/Condicional.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+
+function Condicional() {
+    const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
+
+    const enviarEmail = (e) => {
+        e.preventDefault()
+        setUserEmail(email)
+    }
+
+    const limparEmail = () => {
+        setUserEmail('')
+    }
+
+    return (
+        <div>
+            <h2>Cadastre o seu e-mail:</h2>
+            <form>
+                <input 
+                    type="email" 
+                    placeholder="Digite o seu email" 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button onClick={enviarEmail}>Enviar Email</button>
+                {userEmail && ( //condicional
+                    <div>
+                        <p>O email do usuário é: {userEmail}</p>
+                        <button onClick={limparEmail}>Limpar Email</button>
+                    </div>
+                )}
+            </form>
+        </div>
+    )
+}
+
+export default Condicional
